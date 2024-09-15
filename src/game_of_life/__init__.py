acbf468dfa33e410c122162076c6a8430f5fe429
EstebanMunoz <estebanmunoz.madrid@gmail.com>
2024-09-15 20:44:38 -0300

    Tidy up project description and main __init__ file

--- src/game_of_life/__init__.py
+++ src/game_of_life/__init__.py
@@ -1,26 +1,19 @@
 from game_of_life.model.grid import Grid
-from game_of_life.ui.display import GameOfLifeApp
-
-# def main() -> None:
-#     grid = Grid()
-#     grid.set_cell(0, 0, 1)
-#     grid.set_cell(1, 0, 1)
-#     grid.set_cell(0, 1, 1)
-#     grid.set_cell(-1, 1, 1)
-#     grid.set_cell(-1, 2, 1)
-#     grid.set_cell(-1, 3, 1)
-
-#     app = GameOfLifeApp(grid)
-#     app.run()
+from game_of_life.ui.app import GameOfLifeApp
 
 
-grid = Grid()
-grid.set_cell(0, 0, 1)
-grid.set_cell(1, 0, 1)
-grid.set_cell(0, 1, 1)
-grid.set_cell(-1, 1, 1)
-grid.set_cell(-1, 2, 1)
-grid.set_cell(-1, 3, 1)
+def main() -> None:
+    grid = Grid()
+    grid.set_cell(0, 0, 1)
+    grid.set_cell(1, 0, 1)
+    grid.set_cell(0, 1, 1)
+    grid.set_cell(-1, 1, 1)
+    grid.set_cell(-1, 2, 1)
+    grid.set_cell(-1, 3, 1)
 
-app = GameOfLifeApp(grid)
-app.run()
+    app = GameOfLifeApp(grid)
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
